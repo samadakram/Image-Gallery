43e494568129b42e12f217e98e218a194402750e
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-16 10:25:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// Import Components
+import { Header } from './Components/Header/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>Image Gallery</h1>
+      <Header />
     </div>
   );
 }
