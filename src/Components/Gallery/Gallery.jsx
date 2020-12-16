75f9d915b147dd3b9e0d503cd351894ec8b35ba3
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-16 11:35:03 +0500

    Update Gallery.jsx

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 // Imported Compoents from Material Ui
-import { Grid, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 export const Gallery = () => {
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        const ImageGallery = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+            const data = await response.json();
+
+            console.log("Data =>", data);
+            setImages(data);
+        }
+        ImageGallery();
+    }, []);
+
     return (
         <div>
-            <Grid container>
-                <Grid item xs={12} md={6} lg={4}>
-                    <Typography>Image 1</Typography>
+            <Grid container spacing={3}>
+
+                {images.slice(0, 50).map((image, ind) => {
+                    return (
+                        <Grid item xs={12} md={6} lg={4}>
+                            <img key={ind} src={image.url} alt={image} />
+                        </Grid>
+                    )
+                })}
+                {/* <Grid item xs={12} md={6} lg={4}>
+                    <Paper elevation={3}>Paper 2</Paper>
                 </Grid>
                 <Grid item xs={12} md={6} lg={4}>
-                    <Typography>Image 2</Typography>
+                    <Paper elevation={3}>Paper 3</Paper>
                 </Grid>
                 <Grid item xs={12} md={6} lg={4}>
-                    <Typography>Image 3</Typography>
-                </Grid>
-                <Grid item xs={12} md={6} lg={4}>
-                    <Typography>Image 4</Typography>
-                </Grid>
+                    <Paper elevation={3}>Paper 4</Paper>
+                </Grid> */}
             </Grid>
         </div>
     )
