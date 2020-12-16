ad18744761040318ee76d5c997dfb746be2d8465
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-16 10:46:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.module.css';
 
 // Import Components
 import { Header } from './Components/Header/Header';
+import {Gallery} from './Components/Gallery/Gallery';
 
 function App() {
   return (
     <div>
       <Header />
+      <Gallery />
     </div>
   );
 }
