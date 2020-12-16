3333d9f127656d1afa67bb270d935a3f05edc2fa
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-16 17:41:13 +0500

    Update Gallery.jsx

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -19,12 +19,14 @@ export const Gallery = () => {
             const response = await fetch('https://jsonplaceholder.typicode.com/photos');
             const data = await response.json();
 
-            console.log("Data =>", data);
             setImages(data);
         }
         ImageGallery();
     }, []);
 
+    if(!images) {
+        return <h1>Loading...</h1>
+    }
     return (
         <div>
             <Grid container>
