48631196eb6d93184f585ae4366c54b6f091864b
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-16 10:41:44 +0500

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Header.module.css';   // Imported style file
 
 export const Header = () => {
     return (
-        <div>
+        <div className={style.header}>
             <h1>Header</h1>
         </div>
     )
