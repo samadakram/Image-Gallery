3db7d6ceddb898deb664c62412d90735b904f6ed
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-16 11:03:54 +0500

    Update Gallery.jsx

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+
+// Imported Compoents from Material Ui
+import { Grid, Typography } from '@material-ui/core';
+
 export const Gallery = () => {
     return (
         <div>
-            <h1>Gallery Section</h1>
+            <Grid container>
+                <Grid item xs={12} md={6} lg={4}>
+                    <Typography>Image 1</Typography>
+                </Grid>
+                <Grid item xs={12} md={6} lg={4}>
+                    <Typography>Image 2</Typography>
+                </Grid>
+                <Grid item xs={12} md={6} lg={4}>
+                    <Typography>Image 3</Typography>
+                </Grid>
+                <Grid item xs={12} md={6} lg={4}>
+                    <Typography>Image 4</Typography>
+                </Grid>
+            </Grid>
         </div>
     )
 }
